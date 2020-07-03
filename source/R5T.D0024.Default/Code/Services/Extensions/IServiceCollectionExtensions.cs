f7061f6d6b10833ef81a159e4533adc03922b33a
MinexAutomation <minex.automation@gmail.com>
2020-07-03 00:36:52 -0700

    Added R5T.D0024.Default library. Added OSPlatformProvider service implementation class type.

---
+++ source/R5T.D0024.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.D0024.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="OSPlatformProvider"/> implementation of <see cref="IOSPlatformProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddOSPlatformProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IOSPlatformProvider, OSPlatformProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="OSPlatformProvider"/> implementation of <see cref="IOSPlatformProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IOSPlatformProvider> AddOSPlatformProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction.New<IOSPlatformProvider>(() => services.AddOSPlatformProvider());
+            return serviceAction;
+        }
+    }
+}
---
+++ source/R5T.D0024.Default/Code/Services/Implementations/OSPlatformProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace R5T.D0024.Default
+{
+    public class OSPlatformProvider : IOSPlatformProvider
+    {
+        public OSPlatform GetOSPlatform()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return OSPlatform.Windows;
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return OSPlatform.OSX;
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return OSPlatform.Linux;
+            }
+            
+            throw new Exception(@"Unknown operating system (not Windows, OSX, or Linux).");
+        }
+    }
+}
